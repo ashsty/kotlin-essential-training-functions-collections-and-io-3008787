58773b063b73b3b404a0b34878606019ff873732
ashsty <mina.park@deliveryhero.com>
2025-03-12 15:58:54 +0100

    feat: immutable

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,13 @@
 
 fun main() {
+    var ourFirstVariable: Boolean = true
 
+    println(ourFirstVariable)
+
+    ourFirstVariable = false
+
+    println(ourFirstVariable)
+
+    println(ourFirstVariable == true)
+    println(ourFirstVariable == false)
 }
