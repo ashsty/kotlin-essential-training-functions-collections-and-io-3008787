fd15fdbd0981b8d26dd38d81f0433971994b9fb6
ashsty <mina.park@deliveryhero.com>
2025-03-13 16:17:13 +0100

    feat: study string type

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,40 @@
 
 fun main() {
-    
+    val aChar: Char = 'A'
+    val aNumericChar = '0'
+    val newLineChar = '\n'
+
+//    val concatOption1 = 'a' + 'b'
+//    val concatOption2 = 'ab'
+
+    val aString: String = "Hello World"
+    val escaped = "Hello \nWorld"
+    val raw = """
+        Some
+        multiline
+       text
+            is going on
+         here
+    """
+
+    val raw2 = """
+        |Some
+        multiline
+       text
+            |is going on
+         here
+    """.trimMargin("|")
+
+    val concatString = "Hello" + "Kotlin!"
+    val concatNumber = "The number" + 10
+
+    val aNumber = 10
+    val concatTemplate = "The number $aNumber"
+    val concatTemplate2 = "The number ${aNumber + 1}"
+
+    println(concatTemplate)
+    println(concatTemplate2)
+
+    val someString = "Banks Sucks"
+    println(someString.isEmpty())
 }
