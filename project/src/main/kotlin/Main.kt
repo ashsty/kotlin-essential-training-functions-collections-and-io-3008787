ae45743f863d71e9393c37b3505487cd193b7713
ashsty <mina.park@deliveryhero.com>
2025-03-13 16:19:56 +0100

    docs: study

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -4,8 +4,8 @@ fun main() {
     val aNumericChar = '0'
     val newLineChar = '\n'
 
-//    val concatOption1 = 'a' + 'b'
-//    val concatOption2 = 'ab'
+//    val concatOption1 = 'a' + 'b' don't support
+//    val concatOption2 = 'ab' more than 2 letters
 
     val aString: String = "Hello World"
     val escaped = "Hello \nWorld"
@@ -23,18 +23,19 @@ fun main() {
        text
             |is going on
          here
-    """.trimMargin("|")
+    """.trimMargin("|") // remove the margins if the line starts with |
 
     val concatString = "Hello" + "Kotlin!"
-    val concatNumber = "The number" + 10
+    val concatNumber = "The number" + 10 // The number 10
 
     val aNumber = 10
-    val concatTemplate = "The number $aNumber"
-    val concatTemplate2 = "The number ${aNumber + 1}"
+    val concatTemplate = "The number $aNumber" // The number 10
+    val concatTemplate2 = "The number ${aNumber + 1}" // The number 11
 
     println(concatTemplate)
     println(concatTemplate2)
 
-    val someString = "Banks Sucks"
-    println(someString.isEmpty())
+    val someString = " "
+    println(someString.isEmpty()) // false
+    println(someString.isBlank()) // true
 }
