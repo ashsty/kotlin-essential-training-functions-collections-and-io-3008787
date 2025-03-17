32318c0fc2643bb32f1f47f2988d3ba8b623de33
ashsty <mina.park@deliveryhero.com>
2025-03-17 11:36:44 +0100

    docs: study func param

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,17 @@
 
+private fun printGreeting() = println("Hello World!")
+
 fun main() {
-    
+    fun getGreeting(thingToGreet: String) = "Hello $thingToGreet"
+    fun getGreeting2(thingToGreet: Int) = "Hello $thingToGreet"
+    fun getGreeting3(greeting: String, thingToGreet: String) = "$greeting $thingToGreet"
+    fun getGreeting4(greeting: String = "Hi", thingToGreet: String = "Mina") = "$greeting $thingToGreet"
+
+    println(getGreeting("Kotlin")) // Hello Kotlin
+    println(getGreeting2(5)) // Hello 5
+    println(getGreeting3("Hello", "DH")) // Hello DH
+    println(getGreeting4()) // Hi Mina
+    println(getGreeting4("Hey")) // Hey Mina
+    println(getGreeting4(thingToGreet = "DH")) // Hi DH, -> named argument
+    println(getGreeting4(thingToGreet = "DH", greeting = "Hiya")) // Hiya DH -> no need to overload
 }
