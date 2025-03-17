02ec9f19501bfabbd6788d13504f8118dc03b097
ashsty <mina.park@deliveryhero.com>
2025-03-17 11:16:07 +0100

    docs: study basic func

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,17 @@
 
-fun main() {
-    
+private fun getGreeting(): String {
+    return "Hello world!"
+}
+
+/*
+* fun getGreeting() = "Hello world!" -> single expression function
+* fun printGreeting() = println("Hello Kotlin") -> Unit single expression func
+* */
+
+fun main(): Unit {
+    fun getGreeting2() = "Hello Kotlin!" // local function, only can be accessed inside of the main() func
+
+    println(getGreeting())
+    println(getGreeting2())
+    return Unit
 }
