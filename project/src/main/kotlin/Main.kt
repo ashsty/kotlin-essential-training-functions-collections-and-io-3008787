a94c20320016248144af5669bf605109728bebd3
ashsty <mina.park@deliveryhero.com>
2025-03-17 16:09:27 +0100

    docs: study try/catch

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,16 @@
 
 fun main() {
-    
+    val message = try {
+        "The value is ${10 / 0}"
+    } catch (error: Throwable) {
+       "Error: ${error.message}"
+    }
+
+    println(message) // Error: / by zero
+
+    /*try {
+        val message = "The value is ${10 / 0}"
+    } catch (error: Throwable) {
+        println("Error: ${error.message}")
+    }*/
 }
