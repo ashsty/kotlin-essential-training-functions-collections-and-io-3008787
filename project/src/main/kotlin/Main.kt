b40711aad0cfafcc14c317b19cee45f53f93cf75
ashsty <mina.park@deliveryhero.com>
2025-03-17 15:54:05 +0100

    docs: study statement / expression

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,31 @@
 
+fun getMessage(input: Int) = if (input > 3) { //
+        "The value is greater than 3"
+    } else { // has to cover every range
+        "The value is no greater than 3"
+    }
+
+fun getMessageWithWhen(input: Int) = when (input) {
+    3 -> "The value is 3"
+    else -> "The value is not 3"
+}
+
+/*fun getMessage(input: Int): String {
+    return if (input > 3) {
+        "The value is greater than 3"
+    } else {
+        "The value is no greater than 3"
+    }
+}*/
+
 fun main() {
-    
+    val someVariable = 3
+    /*val message = if (someVariable > 3) {
+        "The value is greater than 3"
+    } else {
+        "The value is no greater than 3"
+    }*/
+
+    println(getMessage(someVariable))  // The value is no greater than 3
+    println(getMessageWithWhen(someVariable))  // The value is 3
 }
