3892a736cc1e63fe6bc25a0a8e7127819848e4c0
ashsty <mina.park@deliveryhero.com>
2025-03-17 15:24:05 +0100

    docs: study if and when

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,25 @@
 
 fun main() {
-    
+    val someVariable = 3
+
+    when {
+        someVariable > 3 -> println("$someVariable is greater than 3")
+        someVariable > 2 -> println("$someVariable is greater than 2") // This one will be printed
+        else -> println("No shit")
+    }
+
+    /*if (someVariable > 3) {
+        println("$someVariable is greater than 3")
+    } else if (someVariable > 2) {
+        println("$someVariable is greater than 2")
+    } else {
+        println("No shit")
+    }*/
+
+    when (someVariable) {
+        0, 1 -> println("The value is 0 or 1")
+        2 -> println("The value is 2")
+        3 -> println("The value is 3") // This one will be printed
+        in 4..Int.MAX_VALUE -> println("The value is greater than 4")
+    }
 }
