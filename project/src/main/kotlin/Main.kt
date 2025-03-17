8856c7fa615c9a8f525869fdcf29d924c8b4025f
ashsty <mina.park@deliveryhero.com>
2025-03-17 14:48:39 +0100

    docs: challenge accepted

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,14 @@
 
+fun printFormattedName(firstName: String, lastName: String, nameFormatter: (String, String) -> String) {
+    println(nameFormatter(firstName, lastName))
+}
+
 fun main() {
-    
+    printFormattedName("John", "Mike") { firstName, lastName ->
+        "Mr $firstName $lastName" // Mr John Mike
+    }
+
+    printFormattedName("Mina", "Park") { firstName, lastName ->
+        "Ms $lastName $firstName" // Ms Park Mina
+    }
 }
