0ed76cedb7884a2b24cea6fd1a2364bf9d231cd1
ashsty <mina.park@deliveryhero.com>
2025-03-17 14:41:04 +0100

    docs: study higher-order function

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,14 @@
 
+fun printCalculatedValue(value1: Int, value2: Int, calculator: (Int, Int) -> Int) {
+    println(calculator(value1, value2))
+}
+
 fun main() {
-    
+    printCalculatedValue(10, 12, { value1, value2 -> // higher-order function
+        value1 + value2
+    })
+
+    printCalculatedValue(10, 9) { value1, value2 -> // trailing lambda syntax
+        value1 - value2
+    }
 }
