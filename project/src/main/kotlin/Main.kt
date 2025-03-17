3cb67afce5dc23bc79c53973b9366e21e969f679
ashsty <mina.park@deliveryhero.com>
2025-03-17 16:39:53 +0100

    docs: study loop

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,38 @@
 
 fun main() {
-    
+    var count = 0
+
+    // 1
+    while (count < 5) {
+        print(count)
+        count++
+    }
+
+    do {
+        println(count)
+        count++
+    } while(count < 5)
+    //012345
+
+    //2
+    for(i in 0 until count) print(i)
+
+    println()
+    //012345
+
+    //3
+    for(i in 0 .. count - 1 ) print(i)
+
+    println()
+    //012345
+
+    //4
+    for(i in 10 downTo 0) print(i)
+
+    println()
+    //109876543210
+
+    //5
+    for(i in 10 downTo 0 step 2) print(i)
+    //1086420
 }
