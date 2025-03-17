a85056e925df4bcaf6115bf51f68b986c7d1f2c8
ashsty <mina.park@deliveryhero.com>
2025-03-17 16:59:10 +0100

    docs: accept challenge

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,45 @@
 
-fun main() {
-    
+fun getOutput(input: Any?): String = when (input) {
+    is Number -> {
+        if (input !is Int) {
+            "Input was an Int"
+        } else {
+            "Input was a non-Int Number"
+        }
+    }
+
+    is String -> "Input was a String with length ${input.length}"
+    null -> "Input was null"
+    else -> "Input didn't match target inputs"
 }
+
+/*fun getOutput(input: Any?): String = if (input is Number) {
+    if (input !is Int) {
+        "Input was an Int"
+    } else {
+        "Input was a non-Int Number"
+    }
+}
+else if (input is String) {
+    "Input was a String with length ${input.length}"
+}
+else if (input == null) { "Input was null"}
+else {
+    "Input didn't match target inputs"
+}*/
+
+fun main() {
+    println(getOutput(null))
+    println(getOutput(4))
+    println(getOutput(3.2))
+    println(getOutput("Hello Kotlin"))
+    println(getOutput('a'))
+}
+
+/*
+Input was null
+Input was a non-Int Number
+Input was an Int
+Input was a String with length 12
+Input didn't match target inputs
+*/
