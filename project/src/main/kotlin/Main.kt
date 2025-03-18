14d28b920440e4f5734201b3a07ccc6751fec715
ashsty <mina.park@deliveryhero.com>
2025-03-18 16:46:22 +0100

    docs: study sequences

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,16 @@
-
 fun main() {
-    
+    val languages = listOf("Kotlin", "Java", "c++", "Python")
+        .filter { it.length > 4 } // ["Kotlin", "Python"]
+        .map { it.length }  // 6, 6
+    // cost a lot of resource because "filter" line will be work on every line before the "map" line executed
+
+    // Sequence: depth-first approach
+    // iterable: breadth-first approach
+
+    val languageSequence = sequenceOf("Kotlin", "Java", "c++", "Python")
+
+    val fromIterable = listOf("Kotlin", "Java", "c++", "Python").asSequence()
+
+    languageSequence.filter { it.length > 4 } // ["Kotlin", "Python"]
+        .map { it.length } // 6, 6
 }
