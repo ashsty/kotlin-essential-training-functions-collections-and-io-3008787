c863f4be896ddc2bfcad197506aa282fc8bcba7d
ashsty <mina.park@deliveryhero.com>
2025-03-19 15:22:32 +0100

    docs: study receiving user input

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,18 @@
+import java.io.File
 
 fun main() {
-    
+    print("Enter a file name: ")
+    // val filename = readLine()
+    val filename = readLine() ?: "" // handle null
+    println("You entered: $filename") // project/gradle.properties
+
+//    val isValidFile = File(filename).isFile
+// Type mismatch because we can be sure that the entered filename is not null
+    val isValidFile = File(filename).isFile
+
+    if (isValidFile) {
+        println("Your file is valid")
+    } else {
+        println("Your file is not valid")
+    }
 }
