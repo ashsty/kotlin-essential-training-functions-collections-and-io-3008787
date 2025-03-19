638baf0fc3aa75250bbe4e93563584a3aa3d7d63
ashsty <mina.park@deliveryhero.com>
2025-03-19 15:36:02 +0100

    docs: study file I/O

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,13 @@
+import java.io.File
+import kotlin.io.path.writeLines
 
 fun main() {
+    val scoresFile = File("scores.txt")
 
+    scoresFile.forEachLine { line-> println(line) }
+
+    val sortedLine = scoresFile.readLines().sorted()
+
+    val outputFile = File("sorted_scores.txt").toPath()
+    outputFile.writeLines(sortedLine)
 }
